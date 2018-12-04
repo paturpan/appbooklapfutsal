c6a590ed3342a83318532be4ef7b8bc9f5f58299
unknown <ftr.azka2015@gmail.com>
2018-12-04 19:09:21 +0700

    project asli

---
+++ src/Aplikasi/tftanggal.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Aplikasi;
+
+/**
+ *
+ * @author Cab Priok
+ */
+class tftanggal {
+
+    static void setText(String format) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static String getText() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
